c13854cbbd466d47e512ffda004de4cf098cfe37
Arun K Viswanathan <github@element77.com>
2017-06-26 11:57:17 -0700

    Initial network without image augmentation
    Received score of 0.95486 on Kaggle

--- script/script.py
+++ script/script.py
@@ -1,14 +1,41 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+from keras.models import model_from_json
+import numpy as np 
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# Training and test data 
+test_file = '../input/test.csv'
+model_file = '../output/digitrecognizer.model.json'
+model_weights_file = '../output/digitrecognizer.model.best.hdf5'
+output_file = '../output/predictions.csv'
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+# load json and create model
+json_file = open(model_file, 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+#
+# load the weights that yielded the best validation accuracy
+model.load_weights(model_weights_file)
 
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
+# Load evaluation data from a file and return X
+def load_eval_data(file_name):
+    raw_data = np.genfromtxt(file_name, delimiter=',', skip_header=1)
 
-# Any results you write to the current directory are saved as output.
+    raw_sample_image = raw_data[0]
+    image_size = int(np.sqrt(len(raw_sample_image)))
+    X_shape = (len(raw_data), image_size, image_size, 1)
+
+    X_data = np.zeros(X_shape)
+    for index, datum in enumerate(raw_data):
+        X_data[index] = np.array(datum/255).reshape(image_size, image_size, 1)
+
+    return X_data
+X_eval = load_eval_data(test_file)
+
+# Get predictions on the evaluation data
+with open(output_file, 'w') as f:
+    f.write('ImageId,Label\n')
+    y_eval = model.predict(X_eval)
+    for index, y_hat in enumerate(y_eval):
+        prediction = np.argmax(y_hat)
+        f.write(str(index+1)+','+str(prediction)+'\n')
+    f.close()
